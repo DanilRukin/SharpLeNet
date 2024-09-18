e8c4b1e4863dbc544daff4b9e43f7384f3bae10d
Rukin Danil <rukin2018d@gmail.com>
2024-09-18 21:34:24 +0700

    #2 Реализация тензора

---
+++ src/LeNet/Tensors/Tensor.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeNet.Tensors
+{
+    public class Tensor
+    {
+        public TensorSize Size { get; private set; }
+
+        /// <summary>
+        /// Произведение глубины на ширину
+        /// </summary>
+        private readonly int DW;
+
+        private List<double> _values;
+
+        public Tensor(TensorSize size)
+        {
+            Size = size;
+            DW = size.Depth * Size.Width;
+            _values = new List<double>(Size.Width * Size.Depth * Size.Height);
+        }
+
+        public Tensor(int heigth, int width, int depth):
+            this(new TensorSize() { Height = heigth, Width = width, Depth = depth })
+        { 
+        }
+
+        public double this[int height, int width, int depth]
+        {
+            get => _values[height * DW + width + Size.Depth + depth];
+            set => _values[height * DW + width + Size.Depth + depth] = value;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder builder = new StringBuilder();
+            for (int d = 0; d < Size.Depth; d++)
+            {
+                for (int i = 0; i < Size.Height; i++)
+                {
+                    for (int j = 0; j < Size.Width; j++)
+                        builder.Append(_values[i * DW + j * Size.Depth + d] + " ");
+                    builder.AppendLine();
+                }
+                builder.AppendLine();
+            }
+            return builder.ToString();
+        }
+    }
+}
---
+++ src/LeNet/Tensors/TensorSize.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeNet.Tensors
+{
+    public struct TensorSize
+    {
+        private int _height;
+        public int Height
+        {
+            get => _height;
+            set
+            {
+                ThrowIfInvalid(value);
+                _height = value;
+            }
+        }
+
+        private int _width;
+        public int Width
+        {
+            get => _width;
+            set
+            {
+                ThrowIfInvalid(value);
+                _width = value;
+            }
+        }
+
+        private int _depth;
+        public int Depth
+        {
+            get => _depth;
+            set
+            {
+                ThrowIfInvalid(value);
+                _depth = value;
+            }
+        }
+
+        private static void ThrowIfInvalid(int value, [CallerMemberName] string propertyName = "")
+        {
+            if (value < 0)
+                throw new ArgumentException($"Значение не может быть меньше 0 ({propertyName} = {value})");
+        }
+    }
+}
