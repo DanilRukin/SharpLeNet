9864922aeed8894f8605ec28b4c132436f3a6cd9
Rukin Danil <rukin2018d@gmail.com>
2024-09-24 18:26:08 +0700

    #3 Реализация сверточного слоя - основные функции слоя

---
+++ src/LeNet/Layers/ConvolutionLayer.cs
@@ -0,0 +1,127 @@
+﻿using Accord.Math.Random;
+using LeNet.Services;
+using LeNet.Tensors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeNet.Layers
+{
+    public class ConvolutionLayer
+    {
+        /// <summary>
+        /// Размер входа
+        /// </summary>
+        private TensorSize _inputSize;
+
+        /// <summary>
+        /// Размер выхода
+        /// </summary>
+        private TensorSize _outputSize;
+
+        /// <summary>
+        /// Фильтры (W)
+        /// </summary>
+        private List<Tensor> Filters { get; set; }
+
+        /// <summary>
+        /// Смещения (b)
+        /// </summary>
+        private List<double> Bias { get; set; }
+
+        /// <summary>
+        /// Градиент фильтров (dW)
+        /// </summary>
+        private List<Tensor> _dFilters { get; set; }
+
+        /// <summary>
+        /// Градиент смещений (db)
+        /// </summary>
+        private List<double> _dBias { get; set; }
+
+        /// <summary>
+        /// Паддинг P
+        /// </summary>
+        private int _padding;
+
+        /// <summary>
+        /// Шаг свертки S
+        /// </summary>
+        private int _stride;
+
+        /// <summary>
+        /// Количество фильтров fc
+        /// </summary>
+        private int _filtersCount;
+
+        /// <summary>
+        /// Размер фильтров fs
+        /// </summary>
+        private int _filtersSize;
+
+        /// <summary>
+        /// Глубина фильтров fd
+        /// </summary>
+        private int _filterDepth;
+
+        public ConvolutionLayer(TensorSize size, int filtersCount, int filtersSize, int padding, int stride)
+        {
+            _inputSize = size;
+
+            _outputSize = new TensorSize()
+            {
+                Width = (size.Width - filtersSize + 2 * padding) / stride + 1,
+                Depth = filtersCount,
+                Height = (size.Height - filtersSize + 2 * padding) / stride + 1
+            };
+
+            _padding = padding;
+            _stride = stride;
+
+            _filtersCount = filtersCount;
+            _filtersSize = filtersSize;
+            _filterDepth = size.Depth;
+
+            Filters = Enumerable.Repeat(new Tensor(_filtersSize, _filtersSize, _filterDepth), _filtersCount).ToList();
+            _dFilters = Enumerable.Repeat(new Tensor(_filtersSize, _filtersSize, _filterDepth), filtersCount).ToList();
+
+            Bias = Enumerable.Repeat(0d, _filtersCount).ToList();
+            _dBias = Enumerable.Repeat(0d, _filtersCount).ToList();
+
+            InitializeWeights();
+        }
+
+
+        private void InitializeWeights()
+        {
+            RandomNormal random = new RandomNormal();
+            // проходимся по каждому из фильтров
+            for (int index = 0; index < _filtersCount; index++)
+            {
+                for (int i = 0; i < _filtersSize; i++)
+                {
+                    for (int j = 0; j < _filtersSize; j++)
+                    {
+                        for (int k = 0; k < _filterDepth; k++)
+                        {
+                            Filters[index][i, j, k] = random.NextDouble(); // генерируем случайное число и записываем его в элемент фильтра
+                        }
+                    }
+                }
+                Bias[index] = 0.01; // все смещения устанавливаем в 0.01
+            }
+        }
+
+        public Tensor Forward(Tensor input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Tensor Backward(Tensor input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/LeNet/Services/RandomNormal.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeNet.Services
+{
+    /// <summary>
+    /// Генератор случайных чисел с нормальным распределением
+    /// </summary>
+    internal class RandomNormal : Random
+    {
+        private double cache;
+        private bool isCacheFilled = false;
+
+        const double mean = 0.5;
+        const double standardDeviation = 0.5 / Math.PI;
+
+        private double Trunc(double value)
+        {
+            if (value < 0.0 || value >= 1.0) return this.Sample();
+            return value;
+        }
+
+        protected override double Sample()
+        {
+            if (isCacheFilled)
+            {
+                isCacheFilled = false;
+                return cache;
+            }
+
+            double r = 0.0;
+            double x = 0.0;
+            double y = 0.0;
+
+            do
+            {
+                x = 2.0 * base.Sample() - 1.0;
+                y = 2.0 * base.Sample() - 1.0;
+                r = x * x + y * y;
+            }
+            while (r >= 1.0 || r == 0.0);
+
+            double z = Math.Sqrt(-2.0 * Math.Log(r) / r);
+
+            cache = Trunc(mean + standardDeviation * x * z);
+            isCacheFilled = true;
+
+            return Trunc(mean + standardDeviation * y * z);
+        }
+    }
+}
