509f1e61f8deb932537f033f688314c1f2fab5c9
Rukin Danil <rukin2018d@gmail.com>
2026-01-25 18:32:57 +0700

    DEVELOPMENT-27: Вычислительный движок (Autograd Engine) - Реализация алгоритма автоматического дифференцирования

--- src/SharpLeNet.Core/Tensor.cs
+++ src/SharpLeNet.Core/Tensor.cs
@@ -39,7 +39,28 @@ public class Tensor
     /// </summary>
     public Tensor? Grad { get; set; }
 
-    public Tensor(double[] data, int[] shape)
+    /// <summary>
+    /// Левый операнд (родительский тензор)
+    /// </summary>
+    public Tensor? LeftParent { get; private set; }
+
+    /// <summary>
+    /// Правый операнд (родительский тензор)
+    /// </summary>
+    public Tensor? RightParent { get; private set; }
+
+    /// <summary>
+    /// Операция, породившая тензор
+    /// </summary>
+    public TensorOperation Operation { get; private set; }
+
+    /// <summary>
+    /// Указывает, вычислен ли градиент
+    /// </summary>
+    public bool RequiresGrad { get; private set; }
+
+    public Tensor(double[] data, int[] shape, Tensor? left, Tensor? right,
+        TensorOperation operation, bool requiresGrad = false)
     {
         if (data.Length != shape.Aggregate(1, (a, b) => a * b))
             throw new ArgumentException("Общее количество элементов должно совпадать с произведением размерностей!");
@@ -47,9 +68,27 @@ public class Tensor
         Shape = (int[])shape.Clone();
 
         ComputeStrides();
+
+        LeftParent = left;
+        RightParent = right;
+        Operation = operation;
+        RequiresGrad = requiresGrad 
+            || (LeftParent?.RequiresGrad == true) 
+            || (RightParent?.RequiresGrad == true);
+
+        if (RequiresGrad)
+        {
+            Grad = new Tensor(shape);
+        }
     }
 
-    public Tensor(int[] shape) : this(new double[shape.Aggregate(1, (a, b) => a * b)], shape)
+    public Tensor(double[] data, int[] shape, bool requiresGrad = false) 
+        : this(data, shape, null, null, TensorOperation.None, requiresGrad)
+    {
+    }
+
+    public Tensor(int[] shape, bool requiresGrad = false) 
+        : this(new double[shape.Aggregate(1, (a, b) => a * b)], shape, requiresGrad)
     {
     }
 
@@ -174,4 +213,133 @@ public class Tensor
         result.Fill(1.0);
         return result;
     }
+
+    /// <summary>
+    /// Обратное распространение ошибки
+    /// </summary>
+    /// <param name="gradient">Градиент</param>
+    public void Backward(Tensor? gradient = null)
+    {
+        if (!RequiresGrad)
+            return;
+
+        // Если gradient == null и это скаляр (размер = 1), инициализируем как 1.0
+        if (gradient == null)
+        {
+            if (Size != 1)
+                throw new InvalidOperationException("Градиент может быть создан " +
+                    "для скалярного значения");
+            Grad!.Fill(1.0);
+        }
+        else
+        {
+            // Суммируем градиенты (для случая, когда тензор используется несколько раз)
+            AddGradient(gradient);
+        }
+
+        BackwardToParents();
+    }
+
+    /// <summary>
+    /// Выполняет добавление значений градиента к текущим значениям
+    /// в <see cref="Data"/> тензора
+    /// </summary>
+    /// <param name="gradient">Градиент для добавления</param>
+    /// <exception cref="ArgumentException"></exception>
+    private void AddGradient(Tensor gradient)
+    {
+        if (!Shape.SequenceEqual(gradient.Shape))
+            throw new ArgumentException("Кол-во измерений и их размерность градиента " +
+                "должна сопадать с кол-вом измерений и их размерностью у данного экземпляра");
+        for (int i = 0; i < Size; i++)
+        {
+            Grad!.Data[i] += gradient.Data[i];
+        }
+    }
+
+    /// <summary>
+    /// В зависимости от операции, вычисляет градиенты для родителей
+    /// </summary>
+    private void BackwardToParents()
+    {
+        switch (Operation)
+        {
+            case TensorOperation.Add:
+                // d(L)/dA = d(L)/dC * 1
+                // d(L)/dB = d(L)/dC * 1
+                LeftParent?.Backward(Grad);
+                RightParent?.Backward(Grad);
+                break;
+            case TensorOperation.Mul:
+                // d(L)/dA = d(L)/dC * B
+                // d(L)/dB = d(L)/dC * A
+                if (LeftParent != null && RightParent != null)
+                {
+                    Tensor gradForLeft = Grad! * RightParent;
+                    Tensor gradForRight = Grad! * LeftParent;
+                    LeftParent.Backward(gradForLeft);
+                    RightParent.Backward(gradForRight);
+                }
+                break;
+
+            // Другие операции добавим позже
+            case TensorOperation.MatMul:
+                // dL/dA = dL/dC @ B^T
+                // dL/dB = A^T @ dL/dC
+                if (LeftParent != null && RightParent != null)
+                {
+                    Tensor gradForLeft = Grad!.MatMul(RightParent.Transpose());
+                    Tensor gradForRight = LeftParent.Transpose().MatMul(Grad!);
+                    LeftParent.Backward(gradForLeft);
+                    RightParent.Backward(gradForRight);
+                }
+                break;
+
+            case TensorOperation.Sigmoid:
+                // d(L)/dx = d(L)/dσ * σ'(x)
+                // где σ'(x) = σ(x) * (1 - σ(x))
+                if (LeftParent != null)
+                {
+                    // Вычисляем σ'(x) = output * (1 - output)
+                    // где output = this (результат сигмоиды)
+                    double[] sigmaPrimeData = new double[Size];
+                    for (int i = 0; i < Size; i++)
+                    {
+                        sigmaPrimeData[i] = Data[i] * (1 - Data[i]);
+                    }
+                    Tensor sigmaPrime = new(sigmaPrimeData, Shape);
+
+                    // Умножаем градиент на производную
+                    Tensor gradForParent = Grad! * sigmaPrime;
+                    LeftParent.Backward(gradForParent);
+                }
+                break;
+
+            case TensorOperation.Neg:
+                // d(L)/dx = d(L)/d(-x) * (-1)
+                if (LeftParent != null)
+                {
+                    Tensor negativeGrad = new(Shape);
+                    negativeGrad.Fill(-1.0);
+                    Tensor gradForParent = Grad! * negativeGrad;
+                    LeftParent.Backward(gradForParent);
+                }
+                break;
+
+            default:
+                // Листовой узел (исходные данные) - не имеет родителей
+                break;
+        }
+    }
+
+    /// <summary>
+    /// Обнуляет градиент
+    /// </summary>
+    public void ZeroGrad() => Grad?.Fill(0.0);
+
+    public static Tensor operator +(Tensor a, Tensor b) => a.Add(b);
+
+    public static Tensor operator *(Tensor a, Tensor b) => a.Mul(b);
+
+    public static Tensor operator -(Tensor a) => a.Neg();
 }
---
+++ src/SharpLeNet.Core/TensorOperations.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpLeNet.Core;
+
+public static class TensorOperations
+{
+    /// <summary>
+    /// Операция сложения тензоров
+    /// </summary>
+    /// <param name="a">Левый операнд</param>
+    /// <param name="b">Правый операнд</param>
+    public static Tensor Add(this Tensor a, Tensor b)
+    {
+        if (!a.Shape.SequenceEqual(b.Shape))
+            throw new ArgumentException("Кол-во измерений и их размерности у слагаемых " +
+                "должны совпадать!");
+        double[] resultData = new double[a.Size];
+        for (int i = 0; i <  a.Size; i++)
+        {
+            resultData[i] = a.Data[i] + b.Data[i];
+        }
+        return new Tensor(resultData, a.Shape, a, b, TensorOperation.Add, 
+            a.RequiresGrad || b.RequiresGrad);
+    }
+
+    /// <summary>
+    /// Операция умножения тензоров (поэлементная)
+    /// </summary>
+    /// <param name="a">Левый операнд</param>
+    /// <param name="b">Правый операнд</param>
+    /// <exception cref="ArgumentException"></exception>
+    public static Tensor Mul(this Tensor a, Tensor b)
+    {
+        if (!a.Shape.SequenceEqual(b.Shape))
+            throw new ArgumentException("Кол-во измерений и их размерности у множителей " +
+                "должны совпадать!");
+        double[] resultData = new double[a.Size];
+        for (int i = 0; i < a.Size; i++)
+        {
+            resultData[i] = a.Data[i] * b.Data[i];
+        }
+        return new Tensor(resultData, a.Shape, a, b,
+            TensorOperation.Mul, a.RequiresGrad || b.RequiresGrad);
+    }
+
+    /// <summary>
+    /// Операция матричного умножения тензоров
+    /// </summary>
+    /// <param name="a">Левый операнд</param>
+    /// <param name="b">Правый операнд</param>
+    /// <exception cref="ArgumentException"></exception>
+    public static Tensor MatMul(this Tensor a, Tensor b)
+    {
+        if (a.Rank != 2 || b.Rank != 2)
+            throw new ArgumentException("Операция матричного умножения может быть" +
+                " выполнена только для матриц!");
+        if (a.Shape[1] != b.Shape[0])
+            throw new ArgumentException($"Кол-во столбцов первой матрицы должно " +
+                $"совпадать с кол-вом строк второй матрицы! Размерности текущих матриц: " +
+                $"[{a.Shape[0]}, {a.Shape[1]}] x [{b.Shape[0]}, {b.Shape[1]}]");
+
+        int m = a.Shape[0];
+        int n = a.Shape[1];
+        int p = b.Shape[1];
+
+        double[] resultData = new double[m * p];
+        int[] resultShape = new int[] { m, p };
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < p; j++)
+            {
+                double sum = 0;
+                for (int k = 0; k < n; k++)
+                {
+                    sum += a[i, k] * b[k, j];
+                }
+                resultData[i * p + j] = sum;
+            }
+        }
+
+        return new Tensor(resultData, resultShape, a, b, TensorOperation.MatMul,
+            a.RequiresGrad || b.RequiresGrad);
+    }
+
+    /// <summary>
+    /// Операция вычисления сигмоиды
+    /// </summary>
+    /// <param name="a">Тензор, для которго выполняется операция</param>
+    public static Tensor Sigmoid(this Tensor a)
+    {
+        var resultData = new double[a.Size];
+        for (int i = 0; i < a.Size; i++)
+        {
+            resultData[i] = 1.0 / (1.0 + Math.Exp(-a.Data[i]));
+        }
+
+        return new Tensor(resultData, a.Shape, a, null, TensorOperation.Sigmoid,
+            a.RequiresGrad);
+    }
+
+    /// <summary>
+    /// Операция сложения всех элементов тензора. 
+    /// В результате - скалярная сумма всех элементов
+    /// </summary>
+    /// <param name="a">Тензор, для которого выполняется операция</param>
+    public static Tensor Sum(this Tensor a)
+    {
+        double sum = 0;
+        for (int i = 0; i < a.Size; i++)
+        {
+            sum += a.Data[i];
+        }
+
+        return new Tensor([sum], [1], a, null, 
+            TensorOperation.Sum, a.RequiresGrad);
+    }
+
+    /// <summary>
+    /// Операция отрицания
+    /// </summary>
+    /// <param name="a">Тензор, для которого выполняется отрицание</param>
+    public static Tensor Neg(this Tensor a)
+    {
+        double[] result = new double[a.Size];
+        for (int i = 0; i < a.Size; i++)
+        {
+            result[i] = -a.Data[i];
+        }
+
+        return new Tensor(result, a.Shape, a, null, TensorOperation.Neg,
+            a.RequiresGrad);
+    }
+}
